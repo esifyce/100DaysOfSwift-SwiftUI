d647720d9d08d61e8256c0351a1fc12707b7a0df
esifyce <49156359+esifyce@users.noreply.github.com>
2022-05-13 17:44:03 +0600

    Add files via upload

---
+++ 17.Day-Project1-WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Sabir Myrzaev on 12/5/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
