8bbd3e7742eb8228d87d081c28619486eabac5d6
esifyce <49156359+esifyce@users.noreply.github.com>
2022-05-19 18:38:34 +0600

    Add files via upload

---
+++ 23.Day-Project3-ViewAndModifiers/ViewAndModifiers/ContentView.swift
@@ -0,0 +1,85 @@
+//
+//  ContentView.swift
+//  ViewAndModifiers
+//
+//  Created by Sabir Myrzaev on 19/5/22.
+//
+
+import SwiftUI
+
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    @ViewBuilder let content: (Int, Int) -> Content
+
+    var body: some View {
+        VStack {
+            ForEach(0..<rows, id: \.self) { row in
+                HStack {
+                    ForEach(0..<columns, id: \.self) { column in
+                        content(row, column)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct Watermark: ViewModifier {
+    var text: String
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(.black)
+        }
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        modifier(Watermark(text: text))
+    }
+}
+
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .padding()
+            .background(.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            GridStack(rows: 4, columns: 4) { row, col in
+                Image(systemName: "\(row * 4 + col).circle")
+                Text("R\(row) C\(col)")
+            }
+            Color.blue
+                .frame(width: 300, height: 200)
+                .watermarked(with: "Hacking with swift")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 23.Day-Project3-ViewAndModifiers/ViewAndModifiers/ViewAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewAndModifiersApp.swift
+//  ViewAndModifiers
+//
+//  Created by Sabir Myrzaev on 19/5/22.
+//
+
+import SwiftUI
+
+@main
+struct ViewAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
